bc9890ac8d3dbd8efe621f8d8aa178ea3455d825
William Li <email@williamli.dev>
2022-03-29 10:37:08 +0800

    set rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/journal',
+        destination: `https://75523-nextjs-with-trailing-slash.vercel-support.app/journal/`,
+      },
+      {
+        source: '/journal/:path*',
+        destination: `https://75523-nextjs-with-trailing-slash.vercel-support.app/journal/:path*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
