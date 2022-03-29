899989d049c908f4aa85b1f6c2d3b4203122b90c
William Li <email@williamli.dev>
2022-03-29 10:50:34 +0800

    trying to hardcode no slash to slash

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
+  async redirect() {
     return [
       {
-        source: '/:path*',
-        destination: `/:path*`,
-      },
-      {
-        source: '/journal',
-        destination: `https://75523-nextjs-with-trailing-slash.vercel-support.app/journal`,
-      },
-      {
-        source: '/journal/:path*',
-        destination: `https://75523-nextjs-with-trailing-slash.vercel-support.app/journal/:path*`,
-      },
+        source: '/docs',
+        destination: `/docs/`,
+      }
     ]
   },
 }
